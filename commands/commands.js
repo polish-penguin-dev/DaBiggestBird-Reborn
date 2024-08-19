1356953661bac4c57d7e92aaff8fa53b349ed298
penguin dev <74113025+polish-penguin-dev@users.noreply.github.com>
2024-08-19 20:51:25 +0100

    Add much more commands

--- commands/commands.js
+++ commands/commands.js
@@ -46,5 +46,106 @@ export const commands = [
             }
         ],
         dmPermission: false
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "shit",
+        description: "Shit To Lose KG And Earn BS!",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.STRING,
+                name: "amount",
+                description: "Enter Amount Of KG To Shit (Or All)",
+                required: true
+            }
+        ],
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "poop",
+        description: "Poop To Immediately Lose 1KG and earn 1BS",
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "stats",
+        description: "Check Your Or Another User's Statistics",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.USER,
+                name: "user",
+                description: "Specify If Checking Another User's Statistics",
+            }
+        ],
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "milk",
+        description: "Milk Another User To Gain Milk",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.USER,
+                name: "user",
+                description: "Specify A User To Milk",
+                required: true
+            }
+        ],
+        dmPermission: false
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "sellmilk",
+        description: "Sell Milk On The Dark Web For KG/BS",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.STRING,
+                name: "amount",
+                description: "Enter Amount Of Milk To Sell (Or All)",
+                required: true
+            }, {
+                type: ApplicationCommandOptionTypes.STRING,
+                name: "currency",
+                description: "Enter Currency To Convert Milk Into",
+                choices: [
+                    {
+                        name: "kg",
+                        value: "kg"
+                    }, 
+                    {
+                        name: "bs",
+                        value: "bs"
+                    }
+                ],
+                required: true
+            }
+        ],
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "shop",
+        description: "Buy Something To Progress In The Game",
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "inventory",
+        description: "Check Your Items Purchased From The Shop",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.USER,
+                name: "user",
+                description: "Specify If Checking Another User's Inventory",
+            }
+        ],
+        dmPermission: true
+    }, {
+        type: ApplicationCommandTypes.CHAT_INPUT,
+        name: "rob",
+        description: "Rob Someone To Steal Their BS/Milk Bottles",
+        options: [
+            {
+                type: ApplicationCommandOptionTypes.USER,
+                name: "user",
+                description: "Specify User To Rob",
+                required: true
+            }
+        ],
+        dmPermission: false
     }
 ];
